bb67c809373aa0e5acca488e2d265b5a878b2eca
Pixel Pernille <pernillejapanskestudier@gmail.com>
2025-12-15 16:56:54 +0100

    forsøg på af at lave en slider... yolo

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const pricePerCig = 3.15;  // man kan ændre dette til den ønskede pris
 const frames = ["Images/frame1.png", "Images/frame2.png", "Images/frame3.png"];
 
 const smoker = document.getElementById("smoker");
-const message = document.getElementById("message");
 
 const cigCountDisplay = document.getElementById("cigCount");
 const moneyDisplay = document.getElementById("moneySpent");
@@ -38,4 +37,31 @@ smoker.addEventListener("click", () => {
     coughSound.currentTime = 0;
     coughSound.play();
     }
+});
+
+
+
+// Tand Script :D
+
+// === TAND SCRIPT ===
+const slider = document.getElementById('slider');
+const teeth = document.getElementById('teeth');
+const label = document.getElementById('label');
+
+slider.addEventListener('input', () => {
+  const value = slider.value;
+
+  if (value < 25) {
+    teeth.style.backgroundImage = "url(Images/tand_hvid.png)";
+    label.textContent = "Sunde hvide tænder, ingen snus!";
+  } else if (value < 50) {
+    teeth.style.backgroundImage = "url(Images/tand_let_gul.png)";
+    label.textContent = "Efter 6 måneders normal snus forbrug.";
+  } else if (value < 75) {
+    teeth.style.backgroundImage = "url(Images/tand_gul.png)";
+    label.textContent = "Efter 2 års snus forbrug.";
+  } else {
+    teeth.style.backgroundImage = "url(Images/tand_meget_gul.png)";
+    label.textContent = "Efter 5 års tungt snus forbrug...";
+  }
 });
